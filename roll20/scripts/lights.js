fee9efb13ca5615eeedcfc05becb4f7599327ad2
Samuel Penn <sam@glendale.org.uk>
2016-07-12 22:37:21 +0100

    Added support for toggling lights on and off. Added support for permanent items. Added feature where lamps don't decay until their time is up (unlike torches).

--- roll20/scripts/lights.js
+++ roll20/scripts/lights.js
@@ -24,28 +24,38 @@ on("chat:message", function(msg) {
 
         var commands = msg.content.split(" ");
         var duration = 1;
+        var token = null;
         if (commands.length > 1) {
             duration = commands[1];
+        }
+        if (commands.length > 2) {
+            var token_id = commands[2];
+            token = getObj("graphic", token_id);
         }
 
         var BOX_STYLE="background-color: #EEEE99; color: #000000; padding:0px; border:1px solid black; border-radius: 5px; padding: 5px";
 
         var message = "";
         if (duration < 1) {
-            // Do nothing.
-            return;
-        } else if (duration == 1) {
-            message = "1 minute passes.";
+            // Output nothing.
         } else {
-            message = "" + duration + " minutes pass.";
+            if (duration == 1) {
+                message = "1 minute passes.";
+            } else {
+                message = "" + duration + " minutes pass.";
+            }
+            sendChat("", "<div style='" + BOX_STYLE + "'>" + message + "</div>");
         }
-        sendChat("", "<div style='" + BOX_STYLE + "'>" + message + "</div>");
 
-        var objects = findObjs({
-            _pageid: Campaign().get("playerpageid"),
-            _type: "graphic", _subtype: "token"
-        });
-
+        var objects = null;
+        if (token == null) {
+            objects = findObjs({
+                _pageid: Campaign().get("playerpageid"),
+                _type: "graphic", _subtype: "token", _name: "Light"
+            });
+        } else {
+            objects = [ token ];
+        }
 
         var count = 0;
         for (var i=0; i < objects.length; i++) {
@@ -56,6 +66,12 @@ on("chat:message", function(msg) {
                 if (notes.indexOf("!Light") > -1) {
                     var max = obj.get("bar2_max");
                     var current = obj.get("bar2_value");
+                    var onOff = obj.get("bar3_value");
+
+                    var params = notes.split(" ");
+                    var type = params[1];
+                    var defaultLight = params[2];
+                    var defaultDim = params[3];
 
                     var lightRadius = obj.get("light_radius");
                     var dimRadius = obj.get("light_dimradius");
@@ -64,22 +80,67 @@ on("chat:message", function(msg) {
                     if (obj.get("name").indexOf("Spell") > -1) {
                         spell = true;
                     }
+                    if (duration == 0) {
+                        onOff = 1 - onOff;
+                        obj.set({
+                            bar3_value: onOff
+                        });
+                    }
+
+                    if (onOff == 0 && lightRadius != "") {
+                        notes = "!Light " + type + " " + defaultLight + " " + defaultDim + " " + lightRadius + " " + dimRadius;
+                        obj.set({
+                            gmnotes: notes,
+                            light_radius: "",
+                            light_dimradius: ""
+                        });
+                        continue;
+                    } else if (onOff == 0) {
+                        continue;
+                    } else if (onOff == 1 && lightRadius == "" && params.length > 4) {
+                        lightRadius = params[4];
+                        dimRadius = params[5];
+                    } else if (onOff == 1 && lightRadius == "") {
+                        obj.set({
+                            bar3_value: 0
+                        });
+                        continue;
+                    }
 
                     for (var d=0; d < duration; d++) {
-
                         if (current == 0 && lightRadius == "" && dimRadius == "") {
                             // Nothing to do.
+                            obj.set({
+                                gmnotes: "!Light " + type + " " + defaultLight + " " + defaultDim,
+                                bar3_value: 0
+                            });
                             break;
                         } else if (current == 0 && lightRadius == 0 && dimRadius == 0) {
                             dimRadius = "";
                             lightRadius = "";
-                        } else if (spell) {
-                            // Spells decay at predicatable rate, and end quickly.
+                        } else if (type == "magic") {
+                            // Magic items are considered permanent. Nothing to do.
+                        } else if (type == "spell") {
+                            // Spells decay at predictable rate, and end quickly.
                             if (current == 0) {
                                 lightRadius = Math.floor(lightRadius / 5);
                                 dimRadius = 0;
                             } else {
                                 current--;
+                            }
+                        } else if (type == "lamp") {
+                            // Lamps stay bright until the end.
+                            if (current == 0) {
+                                lightRadius -= 10;
+                                dimRadius -= 10;
+                                if (dimRadius < 0) {
+                                    dimRadius = 0;
+                                }
+                                if (lightRadius < 0) {
+                                    lightRadius = 0;
+                                }
+                            } else {
+                                current -= Math.floor(Math.random()*3);
                             }
                         } else {
                             // Natural light sources are less predictable.
@@ -105,7 +166,11 @@ on("chat:message", function(msg) {
                             }
                         }
                     }
+                    if (current < 0) {
+                        current = 0;
+                    }
                     obj.set({
+                        gmnotes: "!Light " + type + " " + defaultLight + " " + defaultDim + " " + lightRadius + " " + dimRadius,
                         bar2_value: current,
                         light_radius: lightRadius,
                         light_dimradius: dimRadius
