519cd9750e84102111c16636c351667bd5004829
Samuel Penn <sam@glendale.org.uk>
2016-08-06 12:46:39 +0100

    Now checks for character levels in Bard or Skald, and assumes Bardic Knowledge if they are present. This automatically rolls all knowledge skills, regardless of whether the character has any ranks in them.
    Lore skills are unaffected.

--- roll20/scripts/knowledge.js
+++ roll20/scripts/knowledge.js
@@ -5,11 +5,9 @@
  * those skills that a character actually possesses. Every skill uses the same
  * die roll (which is shown first).
  *
- * Designed to work with the Pathfinder character sheet.
- *
- * TODO:
- *   Allow for bards to make untrained knowledge skill checks.
- *   Allow take 10/20.
+ * Designed to work with the Pathfinder character sheet. Bards and Skalds are
+ * automatically detected (by the character classes), and untrained Knowledge
+ * skills are allowed in this case.
  *
  * Usage:
  *   !knowledge @{selected|token_id} [[d20]]
@@ -49,6 +47,9 @@ Knowledge.usage = function() {
     sendChat("", "Usage: !knowledge @{selected|token_id} [[d20]]")
 }
 
+/**
+ * Setup the basic header for the character sheet template.
+ */
 Knowledge.setupTemplate = function(name, character) {
     var template = "&{template:pf_generic}";
 
@@ -60,8 +61,31 @@ Knowledge.setupTemplate = function(name, character) {
     template += "{{color=" + colour + "}}";
     template += " " + roundedFlag + " ";
     template += "{{character_name=" + name + "}}";
+    template += "{{subtitle=Knowledge Skills}}";
 
     return template;
+}
+
+/**
+ * Is this character a bard? If it has any Bard levels, then it is,
+ * and therefore we should allow untrained Knowledge checks.
+ */
+Knowledge.isBard = function(character) {
+    var c = 0;
+    while (c < 10) {
+        var classNameAttr = "class-"+c+"-name";
+
+        var className = getAttrByName(character.id, classNameAttr);
+        if (className != null) {
+            className = className.toLowerCase();
+            if (className == "bard" || className == "skald") {
+                return true;
+            }
+        }
+        c++;
+    }
+
+    return false;
 }
 
 Knowledge.Process = function(msg, player_obj) {
@@ -101,6 +125,8 @@ Knowledge.Process = function(msg, player_obj) {
     var intelligence = getAttrByName(character.id, "INT-mod");
     template += "{{Intelligence (" + intelligence + ")=**" + (d20roll + intelligence) + "**}}";
 
+    var isBard = Knowledge.isBard(character);
+
     // Define all the Knowledge skills, and iterate through the list, outputting
     // only the ones which the character has ranks in.
     var skills = [ "Knowledge-Arcana", "Knowledge-Dungeoneering",
@@ -113,13 +139,12 @@ Knowledge.Process = function(msg, player_obj) {
     for (var s=0; s < skills.length; s++) {
         var skill = skills[s];
         var ranks = getAttrByName(character.id, skill+"-ranks");
-        if (ranks != "" && ranks > 0) {
-            var score = getAttrByName(character.id, skill);
+        if (isBard || (ranks != "" && ranks > 0)) {
+            var score = parseInt(getAttrByName(character.id, skill));
 
             skill = skill.replace("Knowledge-", "");
             template += "{{"+skill+" (" + score + ")=**" + (d20roll + score) + "**}}";
         }
-
     }
 
     // Now find all the lore skills.
@@ -127,7 +152,7 @@ Knowledge.Process = function(msg, player_obj) {
         var skill = "Lore" + ( (l>1)?l:"" );
         var ranks = getAttrByName(character.id, skill+"-ranks");
         if (ranks != "" && ranks > 0) {
-            var score = getAttrByName(character.id, skill);
+            var score = parseInt(getAttrByName(character.id, skill));
             var lore = getAttrByName(character.id, skill+"-name");
 
             template += "{{*"+lore+"* (" + score + ")=**" + (d20roll + score)  + "**}}";
