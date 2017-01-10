b8fd0e60a8786ffc62f877914ebfa32240d5cadc
Samuel Penn <sam@glendale.org.uk>
2017-01-10 22:58:33 +0000

    Added some usage comments and license. Now works as an executable script.

--- roll20/utils/inner_sea_calendar.py
+++ roll20/utils/inner_sea_calendar.py
@@ -1,3 +1,47 @@
+#!/usr/bin/python
+#
+# Simple program to calculate the day of the week and moon phase given an Inner Sea date.
+# Assumes that 1 AR was on a Moonday, and that it was a full moon.
+# Does not work with dates before 1 AR.
+#
+# Usage:
+# Print out a calendar (in HTML) for the entire year:
+#   inner_sea_calendar.py <year>
+#
+# Print out a calendar (as an HTML fragment) for a given month:
+#   inner_sea_calendar.py <year> <month>
+#
+# Simply report the week day and moon phase for a specific day:
+#   inner_sea_calendar.py <year> <month> <day>
+#
+# Copyright (c) 2017, Samuel Penn
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are those
+# of the authors and should not be interpreted as representing official policies,
+# either expressed or implied, of the FreeBSD Project.
+#
+
 import math
 import sys
 
