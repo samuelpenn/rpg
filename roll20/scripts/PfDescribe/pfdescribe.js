08a221f77838ec6ddc28aeb81ebfe1f046bc0035
Samuel Penn <sam@glendale.org.uk>
2017-02-20 14:49:28 +0000

    Added support for adventure descriptions.

--- roll20/scripts/PfDescribe/pfdescribe.js
+++ roll20/scripts/PfDescribe/pfdescribe.js
@@ -45,17 +45,109 @@ on("chat:message", function(msg) {
     if (msg.content.split(" ", 1)[0] === "!describe") {
         var player_obj = getObj("player", msg.playerid);
         Describe.Process(msg, player_obj);
+    } else if (msg.content.split(" ", 1)[0] === "!missions") {
+        var player_obj = getObj("player", msg.playerid);
+        Describe.missions(msg, player_obj);
+    } else if (msg.content.split(" ", 1)[0] === "!mission") {
+        var id = msg.content.split(" ")[1];
+        Describe.mission(msg, player_obj, id);
     }
 });
 
 var Describe = Describe || {};
 
+Describe.BOX_STYLE="background-color: #EEEEDD; color: #000000; padding:0px; border:1px solid black; border-radius: 5px 5px 10px 10px;";
+Describe.TITLE_STYLE="background-color: black; color: #FFFFFF; padding: 1px; font-style: normal; text-align: center; border-radius: 5px 5px 0px 0px;";
+Describe.TEXT_STYLE="padding: 5px; text-align: left; font-weight: normal; font-style: normal";
+
+Describe.getHTML = function(title, image, text) {
+    var html = "<div style='" + Describe.BOX_STYLE + "'>";
+
+    if (title == null) {
+        title = "";
+    }
+
+    html += "<div style='" + Describe.TITLE_STYLE + "'>" + title + "</div>";
+    if (image != null) {
+        html += "<img src='" + image + "' width='100%'/>";
+    }
+    html += "<div style='" + Describe.TEXT_STYLE + "'>" + text + "</div>";
+    html += "</div>";
+
+    return html;
+};
+
+Describe.missionHandout = function(handout, callback) {
+    handout.get("notes", function(notes) {
+        var notes = unescape(notes);
+
+        handout.get("gmnotes", function(gmnotes) {
+            gmnotes = unescape(gmnotes);
+
+            var firstline = gmnotes.replace(/<br>.*/, "");
+            var title = handout.get("name");
+
+            var faction = firstline.replace(/,.*/, "");
+            var reward = firstline.replace(/.*, /g, "");
+
+            var text = "<b>Faction: </b>" + faction + "<br/>";
+            text += "<b>Reward: </b>" + reward + "<br/>";
+            text += "<br/>";
+
+            text += notes;
+            
+            callback.call(this, handout, title, faction, reward, notes);
+
+//            text += "<br>[Details](!mission " + handout.get("_id")+")";
+//            sendChat("", "/desc " + Describe.getHTML(title, null, text));
+        });
+    });
+
+};
+
+Describe.missionDetails = function(handout, title, faction, reward, notes) {
+    var text = "<b>Faction: </b>" + faction + "<br/>";
+    text += "<b>Reward: </b>" + reward + "<br/>";
+    text += "<br/>";
+
+    text += notes;
+
+    sendChat("", "/desc " + Describe.getHTML(title, null, text));
+}
+
+Describe.missionList = function(handout, title, faction, reward, notes) {
+    var text = "[" + title + " (" + faction + ", " + reward+")](!mission " + handout.get("_id") + ")";
+    sendChat("", text);
+}
+
+Describe.mission = function(msg, player_obj, id) {
+    var list = findObjs({
+        _type: "handout",
+        _id: id
+    });
+    var handout = list[0];
+    Describe.missionHandout(handout, Describe.missionDetails);
+};
+
+
+Describe.missions = function(msg, player_obj) {
+    var list = findObjs({
+        _type: "handout",
+        inplayerjournals: "all"
+    });
+    if (list == null || list.length == 0) {
+        sendChat("Jobs Board", "/desc No missions available");
+        return;
+    }
+
+    sendChat("", "The following jobs are currently listed as being available.");
+    for (var i=0; i < list.length; i++) {
+        var handout = list[i];
+        Describe.missionHandout(handout, Describe.missionList);
+    }
+};
 
 Describe.Process = function(msg, player_obj) {
-    var BOX_STYLE="background-color: #EEEEDD; color: #000000; padding:0px; border:1px solid COLOUR; border-radius: 5px 5px 10px 10px;"
-    var TITLE_STYLE="background-color: COLOUR; color: #FFFFFF; padding: 1px; font-style: normal; text-align: center; border-radius: 5px 5px 0px 0px;";
-    var TEXT_STYLE="padding: 5px; text-align: left; font-weight: normal; font-style: normal";
-
     var n = msg.content.split(" ");
     var target = getObj("graphic", n[1]);
     if (target != undefined) {
@@ -69,13 +161,31 @@ Describe.Process = function(msg, player_obj) {
         var character_id = target.get("represents")
         var character = getObj("character", character_id)
         if (character == null) {
-            sendChat("", "/w " + player_obj.get("displayname") + " No character found");
-        } else {
-            var colour = getAttrByName(character.id, 'rolltemplate_color');
-            if (colour == null || colour == "") {
-                colour = "#000000";
+            // This might be a map symbol that links to a handout.
+            // Look for a handout with the same name.
+            var list = findObjs({
+                _type: "handout",
+                name: title
+            });
+            if (list == null) {
+                sendChat("", "/w " + player_obj.get("displayname") + " No character found");
+                return;
             }
-            var image = null
+            var handout = list[0];
+            var image = handout.get("avatar");
+            if (image == "") {
+                image = null;
+            }
+            handout.get("notes", function(notes) {
+                var text = Describe.getHTML(title, image, unescape(notes));
+                if (playerIsGM(player_obj.get("id"))) {
+                    sendChat("", "/desc " + text);
+                } else {
+                    sendChat("", "/w " + player_obj.get("displayname") + " " + text);
+                }
+            });
+        } else {
+            var image = null;
             if (image == null || image == "") {
                 image = character.get("avatar");
             }
@@ -84,20 +194,14 @@ Describe.Process = function(msg, player_obj) {
                     sendChat("", "/w " + player_obj.get("displayname") + " No bio defined");
                 } else {
                     bio = bio.replace(/<br>-- <br>.*/, "");
-                    colour = "black";
-                    var html = "<div style='" + BOX_STYLE.replace("COLOUR", colour) + "'>";
                     if (title != undefined) {
                         var size = getAttrByName(character.id, "size_display");
                         if (size != null && size != "" && size != "Medium") {
                             title += "<br/>(" + size + ")";
                         }
+                    }
 
-                        html += "<div style='" + TITLE_STYLE.replace("COLOUR", colour) + "'>" + title + "</div>";
-                    }
-                    if (image != null) {
-                        html += "<img src='"+image+"' width='100%'/>";
-                    }
-                    html += "<div style='" + TEXT_STYLE.replace(/COLOUR/g, colour) + "'>" + unescape(bio) + "</div>";
+                    var html = Describe.getHTML(title, image, unescape(bio));
 
                     gmnotes = target.get("gmnotes");
                     if (gmnotes != null && gmnotes != "") {
