1bfffe9b966cbb05b7bfb577c46b8b11fde4f94b
Samuel Penn <sam@glendale.org.uk>
2019-05-04 21:54:34 +0100

    Added support for HTML back in. Use --html as the first parameter to output as HTML instead of DokuWiki format. Uses Unicode characters for the Moon graphics.

--- roll20/utils/inner_sea_calendar.py
+++ roll20/utils/inner_sea_calendar.py
@@ -52,11 +52,14 @@ YEAR_LENGTH = sum(MONTH_DAYS)
 LEAP_YEAR = 8
 MOON_PERIOD = 29.5
 
+HTML=False
+
 WEEK = [ "Moonday", "Toilday", "Wealday", "Oathday", "Fireday", "Starday", "Sunday" ]
 
 MONTH = [ "Abadius", "Calistril", "Pharast", "Gozren", "Desnus", "Sarenith", "Erastus", "Arodus", "Rova", "Lamashan", "Neth", "Kuthona" ]
 
 MOON_PHASES = [ "Full Moon", "Waning Gibbous", "Third Quarter", "Waning Crescent", "New Moon", "Waxing Crescent", "First Quarter", "Waxing Gibbous" ]
+MOON_UNICODE = [ "&#x1F315;", "&#x1F316", "&#x1F317", "&#x1F318", "&#x1F311", "&#x1F312", "&#x1F313", "&#x1F314" ]
 PHASE_LENGTH = [ 3, 4, 4, 4, 2, 4, 4, 4 ]
 BRIGHTNESS = [ 3, 2, 2, 1, 0, 1, 2, 2 ]
 
@@ -171,35 +174,90 @@ def calendar(month, year):
     epocStartDay = getEpocDay(1, month, year)
     epocEndDay = getEpocDay(cal[month-1], month, year)
 
-    html = "===== " + MONTH[month - 1] + " =====\n"
+    if (HTML):
+        html = "<h2>" + MONTH[month - 1] + "</h2>\n"
+    else:
+        html = "===== " + MONTH[month - 1] + " =====\n"
 
-    html += "\n"
-    for name in WEEK:
-        html += "^  " + name + "  "
-    html += "^\n"
+    if (HTML):
+        html += "<table>\n<tr>"
+        for name in WEEK:
+            html += "<th>" + name + "</th>"
+        html += "</tr>\n"
+    else:
+        html += "\n"
+        for name in WEEK:
+            html += "^  " + name + "  "
+        html += "^\n"
 
     today = epocStartDay + 1 - getEpocDayOfWeek(epocStartDay)
-    while today <= epocEndDay:
+    
+    if (HTML):
+        while today <= epocEndDay:
+            if getEpocDayOfWeek(today) == 1:
+                html += "<tr>\n"
 
-        if today < epocStartDay:
-            html += "| "
-        else:
-            phase = "{{ " + str(getMoonPhaseIndex(getDayInMonth(today), month, year)) + ".png?24&nolink}}"
-            html += "|" + str(getDayInMonth(today)) + phase + "\\\\ \\\\ "
+            if today < epocStartDay:
+                html += "<td></td>\n"
+            else:
+                phase="<span>" + MOON_UNICODE[getMoonPhaseIndex(getDayInMonth(today), month, year)] + "</span>"
+                html += "<td>" + str(getDayInMonth(today)) + phase + "</td>\n"
 
-        if getEpocDayOfWeek(today) == 7:
-            html += "|\n"
+            if getEpocDayOfWeek(today) == 7:
+                html += "</tr>\n"
 
-        today += 1
+            today += 1
+    else:
+        while today <= epocEndDay:
 
-    html += "\n|"
+            if today < epocStartDay:
+                html += "| "
+            else:
+                phase = "{{ " + str(getMoonPhaseIndex(getDayInMonth(today), month, year)) + ".png?24&nolink}}"
+                html += "|" + str(getDayInMonth(today)) + phase + "\\\\ \\\\ "
 
-    for d in range(0, 7):
-        html += " +++++++++++ |"
-    html += "\n"
+            if getEpocDayOfWeek(today) == 7:
+                html += "|\n"
+
+            today += 1
+
+        html += "\n|"
+
+    if (HTML):
+        print("</table>\n")
+    else:
+        for d in range(0, 7):
+            html += " +++++++++++ |"
+        html += "\n"
 
     return html
 
+def outputCSS(title):
+    print("<html>\n<head>\n<title>" + title + "</title>\n")
+    print("<style>\n")
+    print("table, th, td {\n")
+    print("    border: 1px solid black;\n")
+    print("    border-collapse;\n")
+    print("    font-size: large;\n")
+    print("}\n")
+    print("td {\n")
+    print("  width: 8em;\n")
+    print("  height: 5em;\n")
+    print("  vertical-align: top;\n")
+    print("}\n")
+    print("td span {\n")
+    print("  align: right;\n")
+    print("  float: right;\n")
+    print("}\n")
+    print("</style>\n</head>\n<body>\n")
+
+def outputEnd():
+    print("</body>\n</html>\n")
+
+if (len(sys.argv) > 1):
+    if (sys.argv[1] == "--html"):
+        sys.argv.pop(0)
+        HTML=True
 
 if (len(sys.argv) == 4):
     argDay = int(sys.argv[3])
@@ -211,13 +269,27 @@ elif (len(sys.argv) == 3):
     argMonth = int(sys.argv[2])
     argYear = int(sys.argv[1])
 
+    if (HTML):
+        outputCSS(MONTH[argMonth - 1] + " " + str(argYear) + " AR")
+
     print calendar(argMonth, argYear)
+
+    if (HTML):
+        outputEnd()
 elif (len(sys.argv) == 2):
     argYear = int(sys.argv[1])
 
-    print "====== " + str(argYear) + " AR ======\n"
+    if (HTML):
+        outputCSS(str(argYear) + " AR")
+        print "<h1>" + str(argYear) + " AR</h1>\n"
+    else:
+        print "====== " + str(argYear) + " AR ======\n"
 
 
     for month in range(1, 13):
         print calendar(month, argYear)
+
     print "\n"
+
+    if (HTML):
+        outputEnd()
